770c4092ca2344a517af1ec2ab1e872f94cb73ab
Leonardo Lima Cavalcante <leonardo_cavalcante@atlantico.com.br>
2023-05-31 00:40:35 -0300

    feat: add fastendpoints swagger integration

--- Media.Api/Program.cs
+++ Media.Api/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument();
 
 var app = builder.Build();
 app.UseFastEndpoints();
@@ -10,6 +11,7 @@ app.UseFastEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwaggerGen();
 }
 
 app.UseHttpsRedirection();
