a909f559877e3a94f65bc465239d0ad58f9f847a
Leonardo Lima Cavalcante <leonardo_cavalcante@atlantico.com.br>
2023-06-24 01:24:32 -0300

    test: add create device unit tests

--- Media.Api/Program.cs
+++ Media.Api/Program.cs
@@ -1,12 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddFastEndpoints();
 builder.Services.SwaggerDocument();
 
 var app = builder.Build();
-app.UseFastEndpoints();
+app.UseFastEndpoints(c => 
+{
+    c.Endpoints.RoutePrefix = "api";
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
