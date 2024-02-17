6210b12fe1b0324681fdaa0ee3204c445802ea61
Leonardo Lima Cavalcante <leonardo_cavalcante@atlantico.com.br>
2024-02-16 23:58:19 -0300

    feat: add new reservation migrations

---
+++ Media.Api/Migrations/20240217002425_ChangeReservationDateColumn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Media.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeReservationDateColumn : Migration
+    {
+        /// <inheritdoc />
+        # pragma warning disable CA1062
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Reservations",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        # pragma warning disable CA1062
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Reservations",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "EndTime",
+                table: "Reservations",
+                type: "time without time zone",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "StartTime",
+                table: "Reservations",
+                type: "time without time zone",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+    }
+}
